015749fddec1850429dae895e5f6fdf18086a1ee
Kanta Sato <sato.dtecnoweb@gmail.com>
2026-08-13 11:09:35 +0900

    Connect analytics and contact form

---
+++ analytics.js
@@ -0,0 +1,11 @@
+(function () {
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function () { window.dataLayer.push(arguments); };
+  window.gtag('js', new Date());
+  window.gtag('config', 'G-FC8WFJBQVB');
+
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-FC8WFJBQVB';
+  document.head.appendChild(script);
+}());
